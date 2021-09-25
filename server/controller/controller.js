4ddfaf2e08af9e2fbea185d43f8df783a1bb76f3
ddthilindra <ddthilindra@gmail.com>
2021-09-26 00:32:48 +0530

    finished

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -19,67 +19,91 @@ exports.create = (req, res) => {
   //save user in the db
   user
     .save(user)
-    .then(data => {
-      res.send(data);
+    .then((data) => {
+      //res.send(data);
+      req.redirect('/add-user');
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message:err.message || "Some orror acurred while creating a create oparation",
+        message:
+          err.message || "Some orror acurred while creating a create oparation",
       });
     });
 };
 
 //retrive and return  all users/ retrive and terun a single user
-exports.find = (req,res) => {
-    Userdb.find()
-    .then(user =>{
-        res.send(user)
-    })
-    .catch(err=>{
-        res.status(500).send({message:err.message || "Error Occurred while retriving user info!"})
-    })
+exports.find = (req, res) => {
+  if (req.query.id) {
+      const id = req.query.id;
 
+      Userdb.findById(id)
+      .then(data =>{
+        if(!data){
+            res.status(400).send({message: "Not found user id"+id})
+        }else{
+           res.send(data) 
+        }
+      })
+      .catch(err=>{
+        res.status(500).send({message: "Error retriving user with id"+id})
+    })
+  } else {
+    Userdb.find()
+      .then((user) => {
+        res.send(user);
+      })
+      .catch((err) => {
+        res
+          .status(500)
+          .send({
+            message: err.message || "Error Occurred while retriving user info!",
+          });
+      });
+  }
 };
 
 //Update a neew identified user by user id
 exports.update = (req, res) => {
-    if(!req.body){
-        return res
-        .status(400)
-        .send({message:"Data to update can not be empty"})
-    }
-    const id = req.params.id;
-    Userdb.findByIdAndUpdate(id,req.body,{useFindAndModify:false})
-    .then(data =>{
-        if(!data){
-            res.status(404).send({message:`Cannot Update user with ${id}. Maybe user not found`})
-        
-    }else{
-        res.send(data)
-    }
+  if (!req.body) {
+    return res.status(400).send({ message: "Data to update can not be empty" });
+  }
+  const id = req.params.id;
+  Userdb.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then((data) => {
+      if (!data) {
+        res
+          .status(404)
+          .send({
+            message: `Cannot Update user with ${id}. Maybe user not found`,
+          });
+      } else {
+        res.send(data);
+      }
     })
-    .catch(err=>{
-        res.status(500).send({message:"Error Update user info"})
-    })
+    .catch((err) => {
+      res.status(500).send({ message: "Error Update user info" });
+    });
 };
 
 //Delete a user with spesified user id in the request
-exports.delete = (req,res) => {
-    const id=req.params.id;
+exports.delete = (req, res) => {
+  const id = req.params.id;
 
-    Userdb.findByIdAndDelete(id)
-    .then(data=>{
-        if(!data){
-            res.status(404).send({message:`Cannot Delete with id ${id}.Maybe id is wrong`})
-        }else{
-            res.send({
-                message : "User was deleted successfully!"
-            })
-        }
-    })
-    .catch(err =>{
-        res.status(500).send({
-            message:"Could not delete User id="+id
+  Userdb.findByIdAndDelete(id)
+    .then((data) => {
+      if (!data) {
+        res
+          .status(404)
+          .send({ message: `Cannot Delete with id ${id}.Maybe id is wrong` });
+      } else {
+        res.send({
+          message: "User was deleted successfully!",
         });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Could not delete User id=" + id,
+      });
     });
 };
--- server/services/render.js
+++ server/services/render.js
@@ -1,11 +1,31 @@
-exports.homeRoutes = (req,res)=>{
-    res.render('index');
-}
+const axios = require("axios");
+const { response } = require("express");
 
-exports.add_user = (req,res)=>{
-    res.render('add_user');
-}
+exports.homeRoutes = (req, res) => {
+  //Make a get req to /api/users
 
-exports.update_user=(req,res)=>{
-    res.render('update_user');
-}
+  axios
+    .get("http://localhost:3000/api/users")
+    .then(function (response) {
+      //console.log(response.data)
+      res.render("index", { users: response.data }); //users can access on index file
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+};
+
+exports.add_user = (req, res) => {
+  res.render("add_user");
+};
+
+exports.update_user = (req, res) => {
+  axios
+    .get("http://localhost:3000/api/users", { params: { id: req.query.id } })
+    .then(function (userdata) {
+      res.render("update_user", { user: userdata.data });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+};
